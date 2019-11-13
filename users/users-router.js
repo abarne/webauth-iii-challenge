616f0f1187208ae3fda916c01e249627740be190
abarne <celius377@gmail.com>
2019-11-13 14:20:39 -0600

    GET users with authorization token added

--- users/users-router.js
+++ users/users-router.js
@@ -2,3 +2,23 @@ const router = require('express').Router();
 
 const Users = require('./users-model.js');
 const restricted = require('../auth/restricted-middleware.js');
+
+router.get('/', restricted, (req, res) => {
+	Users.find()
+		.then((users) => {
+			res.json(users);
+		})
+		.catch((err) => res.send(err));
+});
+
+module.exports = router;
+
+function checkDepartment(department) {
+	return function(req, res, next) {
+		if (department === req.decodedJwt.department) {
+			next();
+		} else {
+			res.status(403).json({ messgage: 'You do not have access to this' });
+		}
+	};
+}
